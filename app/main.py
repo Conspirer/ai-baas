0a244736d6a36021ed026d17c1805223d3b9eed3
Shreyansh Tiwari <shreylol321@gmail.com>
2025-12-28 20:38:17 +0530

    Init: basic FastAPI with routing

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/hello")
+def hello_message():
+    return {"message": "hello"}
+
+@app.get("/status")
+def status_message():
+    return {"check": "okay"}
+
+@app.get("/square/{number}")
+def square(number : int):
+    return {"number": number, "square": number*number}
+
+@app.get("/greet")
+def greet(name: str = "guest"):
+    return {"greeting": f"Hello, {name}"}
